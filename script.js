1f55ee9c95b0f1f241c3cf83a5edb01d65e6a715
Adrian <64158136+adrianage@users.noreply.github.com>
2021-03-10 02:54:54 -0800

    created js file with dom selectors

---
+++ script.js
@@ -0,0 +1,9 @@
+const toggle = document.getElementById('toggle')
+const toggle = document.getElementById('close')
+const toggle = document.getElementById('open')
+const toggle = document.getElementById('modal')
+
+// Toggle nav
+toggle.addEventListener('click', () =>
+    document.body,classList.toggle('show-navß')
+)
